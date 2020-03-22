a08aeaac17d6308d7214ed7791ccd9319031c43c
Chris McKenzie <kristopolous@yahoo.com>
2020-03-22 02:41:53 -0700

    stuff

--- make-html.sh
+++ make-html.sh
@@ -1,10 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 {
   cat << ENDL
 <style>
 body { font-size:0;margin: 0; padding: 0; background: black; }
-img { width: 25% }
+img { width: 33.3% }
+div.inner {margin-bottom: 2rem;max-height:60rem;overflow-y:auto;border-bottom:4px solid white;width: 95%}
+span { text-indent: 2rem; font-size: 20px; color: white; display: block }
+.show img { display: none }
+.show { background: #333; margin-bottom: 1rem;padding: 1rem; }
 </style>
 ENDL
-  find . -name \*.jpg -or -name \*.gif -or -name \*.png | awk ' { print "<img src="$0">" } ' 
+lastdir=''
+for i in $(find . -name \*.jpg -or -name \*.gif -or -name \*.png); do 
+  dir=$(dirname $i)
+  if [[ "$dir" != "$lastdir" ]]; then
+    [[ -n "$lastdir" ]] && echo "</div></div>"
+    echo "<div class=wrap>"
+    echo "<span>$(basename $dir)</span>"
+    echo "<div class=inner onclick=\"this.classList.toggle('show')\">"
+  fi
+  echo "<img src="$i">"
+  lastdir=$dir
+done
+
+echo "</div>"
 } > index.html
