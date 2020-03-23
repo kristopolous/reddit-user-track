8923b8b96ac0b190a7f9f8470eba12361c851510
Chris McKenzie <kristopolous@yahoo.com>
2020-03-22 23:59:47 -0700

    more style

--- make-html.sh
+++ make-html.sh
@@ -5,11 +5,12 @@
 body { font-size:0;margin: 0; padding: 0; background: black; }
 img { width: 33.3% }
 div.wrap { width: 95% }
-div.inner {margin-bottom: 2rem;max-height:60rem;overflow-y:auto;border-bottom:4px solid white }
+div.inner {margin-bottom: 2rem;max-height:60rem;overflow-y:auto;border-bottom:3px solid #777 }
 span { text-indent: .5rem; font-family:sans;font-size: 20px; color: #aaa;background:#222;padding:0.5rem; display: block }
 .show img { display: none }
-.show { background: #333; margin-bottom: 1rem;padding: 1rem; }
+.show { background: #333; margin-bottom: 0; }
 </style>
+<script src=remember.js></script>
 ENDL
 lastdir=''
 for i in $(find . -name \*.jpg -or -name \*.gif -or -name \*.png); do 
