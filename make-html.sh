b83029d395629c8e461d0ea5275d6faa698ceb5c
Chris McKenzie <kristopolous@yahoo.com>
2020-03-30 02:11:26 -0700

    trying to fix a style

--- make-html.sh
+++ make-html.sh
@@ -2,13 +2,15 @@
 {
   cat << ENDL
 <style>
+a { text-decoration: none; color: #aac }
+a:hover { color: #99f}
 body { font-size:0;margin: 0; padding: 0; background: black; }
 img { width: 33.3% }
 div.wrap { width: 95% }
 div.inner {margin-bottom: 2rem;max-height:60rem;overflow-y:auto;border-bottom:3px solid #777 }
 span { text-indent: .5rem; font-family:sans;font-size: 20px; color: #aaa;background:#222;padding:0.5rem; display: block }
 .show img { display: none }
-.show { background: #333; margin-bottom: 0; }
+div.inner.show { background: #333; margin-bottom: 0; }
 </style>
 <script src=remember.js></script>
 ENDL
@@ -23,8 +25,9 @@ for i in $list; do
   dir=$(dirname $i)
   if [[ "$dir" != "$lastdir" ]]; then
     [[ -n "$lastdir" ]] && echo "</div></div>"
+    user=$(basename $dir)
     echo "<div class=wrap>"
-    echo "<span>$(basename $dir)</span>"
+    echo "<span><a href=https://old.reddit.com/u/$user>$user</a></span>"
     echo "<div class=inner onclick=\"this.classList.toggle('show')\">"
   fi
   echo "<img src="$i">"
