97fc723da9c75c4edfe7c4852dd34b47e4e74557
Chris McKenzie <kristopolous@yahoo.com>
2020-04-19 04:49:25 -0700

    fixing the remove script

--- remove.sh
+++ remove.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [[ -e data/$1 ]]; then 
+  echo "Removed: " $(ls data/$1 | wc -l)
   rm -r data/$1
   sed -i '/'$1'/d' userlist.txt
 else 
   echo $1 >> banlist.txt
 fi
-./blockem.py 
 #./make-html.sh
