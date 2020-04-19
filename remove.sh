546d9695666f60245de02673b009d1aad1bd6cec
Chris McKenzie <kristopolous@yahoo.com>
2020-04-19 04:50:55 -0700

    fixing the remove script

--- remove.sh
+++ remove.sh
@@ -3,7 +3,9 @@ if [[ -e data/$1 ]]; then
   echo "Removed: " $(ls data/$1 | wc -l)
   rm -r data/$1
   sed -i '/'$1'/d' userlist.txt
-else 
-  echo $1 >> banlist.txt
 fi
+echo $1 >> banlist.txt
+sort banlist.txt | uniq > /tmp/blist
+mv /tmp/blist banlist.txt
+
 #./make-html.sh
