b10e466f7609f7a3305d312b2e096bc2988dd1da
Chris McKenzie <kristopolous@yahoo.com>
2020-04-23 18:32:38 -0700

    Making add take multiple args

--- add.sh
+++ add.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-[[ -z "$1" ]] && exit
-echo $1 >> userlist.txt
+while [[ -n "$1" ]]; do
+  [[ -z "$1" ]] && exit
+  echo $1 >> userlist.txt
+  ./pull.py $1
+  shift
+done
 ./blockem.py
-./pull.py $1
