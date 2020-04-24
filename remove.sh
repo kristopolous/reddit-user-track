4be8d61518c6d23a3f65108616c4da9c5159c6ee
Chris McKenzie <kristopolous@yahoo.com>
2020-04-23 18:33:01 -0700

    A multi-arg remove

--- remove.sh
+++ remove.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [[ -e data/$1 ]]; then 
-  echo "Removed: "$(ls data/$1 | wc -l)
-  rm -r data/$1
-  sed -i '/'$1'/d' userlist.txt
-fi
-echo $1 >> banlist.txt
+while [[ -n "$1" ]]; do
+  if [[ -e data/$1 ]]; then 
+    echo "Removed: "$(ls data/$1 | wc -l)
+    rm -r data/$1
+    sed -i '/'$1'/d' userlist.txt
+  fi
+  echo $1 >> banlist.txt
+  shift
+done
+
 sort banlist.txt | uniq > /tmp/blist
 mv /tmp/blist banlist.txt
 ./blockem.py
