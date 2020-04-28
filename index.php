64f207add03b0d82bef340c4dbe360bdbfd8ea4f
Chris McKenzie <kristopolous@yahoo.com>
2020-04-27 22:01:46 -0700

    getting the style/structure to match

--- index.php
+++ index.php
@@ -2,24 +2,37 @@
 <link rel=stylesheet href=style.css />
 <script src=remember.js></script>
 <?php
+
 $now = time();
-foreach(glob("data/*/*.*") as $f) {
-  if (basename($f) == 'urllist.txt') {
-    continue;
-  }
-  
-  $when = filemtime($f);
-  if($now - $when < 86400 / 2) {
-    $what = pathinfo($f);
-    if($what['extension'] == 'mp4') {
-?>
-            <video class=video autoplay loop muted="" nocontrols>
-            <source src=<?=$f?>>
-            </video>
-<?
-    } else {
-      echo "<img title='$f' data-src='$f'>";
+foreach(glob("data/*") as $user) {
+  $is_first = true;
+  foreach(glob("$user/*.*") as $f) {
+    if (basename($f) == 'urllist.txt') {
+      continue;
+    }
+    
+    $when = filemtime($f);
+    if($now - $when < 86400 / 2) {
+      if($is_first) {
+        echo "<div data-user='$user' class='cont wrap'>";
+        echo "<span class=user></span>";
+        echo "<div class=inner>";
+        $is_first = false;
+      }
+      $what = pathinfo($f);
+      if($what['extension'] == 'mp4') {
+        ?>
+          <video class=video autoplay loop muted="" nocontrols>
+          <source src=<?=$f?>>
+          </video>
+        <?
+      } else {
+        echo "<img title='$f' data-src='$f'>";
+      }
     }
   }
+  if(!$is_first) { 
+    echo "</div>";
+    echo "</div>";
+  }
 }
-
