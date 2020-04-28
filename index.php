9235695965c8db37b8e6986b048bae676d3c3287
Chris McKenzie <kristopolous@yahoo.com>
2020-04-27 21:38:51 -0700

    more incremental work

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<!doctype html5>
+<link rel=stylesheet href=style.css />
+<script src=remember.js></script>
 <?php
 $now = time();
 foreach(glob("data/*/*.*") as $f) {
@@ -7,7 +10,16 @@ foreach(glob("data/*/*.*") as $f) {
   
   $when = filemtime($f);
   if($now - $when < 86400 / 2) {
-    echo "<object data=$f></object>";
+    $what = pathinfo($f);
+    if($what['extension'] == 'mp4') {
+?>
+            <video class=video autoplay loop muted="" nocontrols>
+            <source src=<?=$f?>>
+            </video>
+<?
+    } else {
+      echo "<img title='$f' data-src='$f'>";
+    }
   }
 }
 
