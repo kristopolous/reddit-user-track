f8cf3c54f01b0de2d9335a18c104a6f3e7aa05e4
Chris McKenzie <kristopolous@yahoo.com>
2020-06-20 21:29:02 -0700

    fixes to the mp4 puller

--- redgif-pull.sh
+++ redgif-pull.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 start=$PWD
+echo -e "\n" > /tmp/newline
 for i in data/*; do
   cd $i
-  touch donelist.txt
-  for i in $(cat urllist.txt donelist.txt | grep redgifs | sort | uniq -u); do
-    youtube-dl "$i"
-    echo "$i" >> donelist.txt
-  done
+  if [ -e "urllist.txt" ]; then
+    touch donelist.txt
+
+    for f in donelist urllist; do
+      grep -Ev '\w\whttps:' $f.txt > /tmp/list
+      mv /tmp/list $f.txt
+    done
+
+    for j in $(cat urllist.txt /tmp/newline donelist.txt | grep redgifs | sort | uniq -u); do
+      youtube-dl "$j" && echo "$j" >> donelist.txt
+    done
+  fi
   cd $start
 done
