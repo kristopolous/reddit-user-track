8e15057f572f4c7e6fa838e016f87ff49dcf894f
Chris McKenzie <kristopolous@yahoo.com>
2020-10-03 23:34:08 -0700

    trying to fix a computation

--- repl.sh
+++ repl.sh
@@ -3,6 +3,6 @@ while read -e -p ">> " action; do
   user=${action:1}
   action=${action:0:1}
   [[ $action == '+' ]] && ./add.sh $user &
-  [[ $action == '-' ]] && ./remove.sh $user &
+  [[ $action == '-' ]] && ./remove.sh $user 
   echo $(( 1000 * $(cat userlist.txt | wc -l) / $(cat blocked.txt | wc -l) ))% 
 done
