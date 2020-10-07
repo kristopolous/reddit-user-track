416a6463a4518dfa68bc1cd31c1cc98b4ed5c5ef
Chris McKenzie <kristopolous@yahoo.com>
2020-10-06 21:32:33 -0700

    speedup

--- facer.py
+++ facer.py
@@ -16,51 +16,51 @@ def lf(path):
             except:
                 return {}
 
-base = sys.argv[1]
+for userPath in glob.glob('data/*'):
+    user = os.path.basename(userPath)
+    faceMap = lf('data/{}/faces.json'.format(user)) or {}
 
-faceMap = lf('data/{}/faces.json'.format(base)) or {}
+    print(user, end='', flush=True)
+    for imagePath in glob.glob("data/{}/*[jp][np]g".format(user)):
+        print('.', end='', flush=True)
+        filename = os.path.basename(imagePath)
 
-print(base, end='', flush=True)
-for imagePath in glob.glob("data/{}/*[jp][np]g".format(base)):
-    print('.', end='', flush=True)
-    filename = os.path.basename(imagePath)
+        if filename in faceMap:
+            continue
 
-    if filename in faceMap:
-        continue
+        try:
+            image = cv2.imread(imagePath)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=7,
+                minSize=(300, 300),
+                flags = cv2.CASCADE_SCALE_IMAGE
+            )
+            faceMap[filename] = len(faces)
+
+            """
+            print(len(faces), imagePath)
+            if len(faces) > 0:
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.imshow("Faces found", image)
+                cv2.waitKey(0)
+            """
+        except:
+            continue
+
+    print('done')
+
+    with open("data/{}/faces.json".format(user), 'w') as fp:
+        json.dump(faceMap, fp)
 
     try:
-        image = cv2.imread(imagePath)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=7,
-            minSize=(300, 300),
-            flags = cv2.CASCADE_SCALE_IMAGE
-        )
-        faceMap[filename] = len(faces)
+        faceMaster = lf('facemaster.json') or {}
+        faceMaster[user] = sum(faceMap.values()) / len(faceMap.values())
 
-        """
-        print(len(faces), imagePath)
-        if len(faces) > 0:
-            for (x, y, w, h) in faces:
-                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.imshow("Faces found", image)
-            cv2.waitKey(0)
-        """
-    except:
-        continue
-
-print('done')
-
-with open("data/{}/faces.json".format(base), 'w') as fp:
-    json.dump(faceMap, fp)
-
-try:
-    faceMaster = lf('facemaster.json') or {}
-    faceMaster[base] = sum(faceMap.values()) / len(faceMap.values())
-
-    with open("facemaster.json", 'w') as fp:
-        json.dump(faceMaster, fp)
-except: 
-    pass
+        with open("facemaster.json", 'w') as fp:
+            json.dump(faceMaster, fp)
+    except: 
+        pass
