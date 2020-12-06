1435316d557cbe1d37a8ca5765b4aeb8436bd093
Chris McKenzie <kristopolous@yahoo.com>
2020-12-05 22:32:53 -0800

    faster way to in/exclude

--- repl.sh
+++ repl.sh
@@ -2,6 +2,11 @@
 while read -e -p ">> " action; do
   user=${action:1}
   action=${action:0:1}
+  if [[ -z "$user" ]]; then  
+    user=$(xwininfo -root -tree | grep Google\ Chrome | awk '{ print $4 } '
+    )
+    echo "${action}${user}"
+  fi
   [[ $action == '+' ]] && ./add.sh $user &
   [[ $action == '-' ]] && ./remove.sh $user 
   echo $(( 1000 * $(cat userlist.txt | wc -l) / $(cat blocked.txt | wc -l) ))% 
