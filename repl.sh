7f1cbf91afe79c51181f5d6b7fce54d8ad32c3ed
Chris McKenzie <kristopolous@yahoo.com>
2021-05-13 14:51:49 -0700

    supporting different reddit version

--- repl.sh
+++ repl.sh
@@ -3,13 +3,15 @@ while read -e -p ">> " action; do
   user=${action:1}
   action=${action:0:1}
   if [[ -z "$user" ]]; then  
-    user=$(xwininfo -root -tree | grep Google\ Chrome | awk '{ print $4 } '
-    )
+    user=$(xwininfo -root -tree | grep Google\ Chrome | grep -E "(submitted|overview)" | awk '{ print $4 } ' )
+    if [[ -z "$user" ]]; then
+      user=$(xwininfo -root -tree | grep Google\ Chrome | grep -Po "(?<=\(u\/)([^\)]*)" )
+    fi
     echo "${action}${user}"
   fi
   [[ $action == '+' ]] && ./add.sh $user &
   [[ $action == '-' ]] && ./remove.sh $user 
   echo "Following: $(cat userlist.txt | wc -l)"
   echo "  Blocked: $(cat blocked.txt | wc -l)"
-  echo $(( 10000 * $(cat userlist.txt | wc -l) / $(cat blocked.txt | wc -l) ))% 
+  echo $(( 100000 * $(cat userlist.txt | wc -l) / $(cat blocked.txt | wc -l) ))% 
 done
