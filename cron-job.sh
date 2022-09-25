cb6b8e0ac3a270c72468b0635ab0686b98a07f2b
Chris McKenzie <kristopolous@yahoo.com>
2022-09-25 01:33:40 -0700

    updating the cron

--- cron-job.sh
+++ cron-job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+today=$(date +%m%d%H)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./pull.py 
-./redgif-pull.sh && touch last_run.txt
+flock -n /tmp/$today ./pull.py  || exit 0
+#./redgif-pull.sh && touch last_run.txt
