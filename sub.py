b26a18fdd81cc1be9fd713487bec87f8c63d46d9
Chris McKenzie <kristopolous@yahoo.com>
2023-08-16 05:18:36 -0700

    adding another overlooked file

---
+++ sub.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import praw
+import os
+import secrets
+import json
+import sys
+import pdb
+reddit = praw.Reddit(
+    client_id=secrets.reddit['block']['id'], 
+    client_secret=secrets.reddit['block']['secret'], 
+    password=secrets.reddit['block']['password'], user_agent='test', 
+    username=secrets.reddit['block']['username']
+)
+
+def lf(path, kind = 'set'):
+    if os.path.exists(path):
+        with open(path) as fp:
+            if kind == 'json':
+                return json.load(fp)
+
+            return set(fp.read().splitlines())
+
+sub = sys.argv[1]
+data = lf('{}.json'.format(sub), 'json') or {}
+linear = open('{}.txt'.format(sub), 'a')
+
+for submission in reddit.subreddit(sub).new():
+    if submission.name not in data:
+        data[submission.name] = [submission.title, submission.selftext]
+        linear.write(submission.title + "\n--\n")
+        try:
+            linear.write("{}: {}\n".format(submission.author.name, submission.selftext))
+        except:
+            linear.write("(unknown): {}\n".format(submission.selftext))
+
+    comments = submission.comments;
+    comments.replace_more()
+    for comment in comments.list():
+        if comment.name not in data:
+
+            data[comment.name] = comment.body
+            try:
+                linear.write("{}: {}\n".format(comment.author.name,comment.body))
+            except:
+                linear.write("(unknown): {}\n".format(comment.body))
+
+    with open("{}.json".format(sub), 'w') as fp:
+        json.dump(data, fp)
+
+linear.close()
