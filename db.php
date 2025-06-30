9ec6f048c7f3c13d4fef690556ef20cb3211c717
Chris McKenzie <kristopolous@yahoo.com>
2025-06-30 02:02:16 +0100

    file -> redis

--- db.php
+++ db.php
@@ -1,28 +1,15 @@
 <?php
-$_db = 'rating.json';
 
 function db($key = false, $value = false) {
-  global $_db;
-  $db = [];
-  if(file_exists($_db)) {
-    $raw = file_get_contents($_db);
-    $db = json_decode($raw, true);
-    if(!$db) {
-      if(strlen($raw) < 10) {
-        $db = [];
-      } else {
-        // we want to avoid corrupting things
-        return;
-      } 
-    }
-  }
+  $redis = new Redis();
+  $obj = 'rating';
+  $db = $redis->hgetall($obj) ?? [];
 
   switch(func_num_args()) {
     case 0: return $db; break;
-    case 1: return isset($_db[$key]) ? $_db[$key] : null; break;
+    case 1: return isset($db[$key]) ? $db[$key] : null; break;
     case 2:
-      $db[$key] = $value;
-      file_put_contents($_db, json_encode($db));
+      $redis->hset($obj, $key, $value);
       break;
   }
 }
