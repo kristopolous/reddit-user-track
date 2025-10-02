bfcf0760009ecb071de8437c2b45156fbf672d81
Chris McKenzie <kristopolous@yahoo.com>
2025-10-03 00:30:27 +0100

    extending the format check

--- lib.php
+++ lib.php
@@ -3,7 +3,7 @@
 function is_video($path) {
    $parts = explode('?', $path);
    $what = pathinfo($parts[0]);
-   if (in_array($what['extension'], ['mp4', 'flv', 'gifv', 'gif', 'webm'])) {
+   if (in_array($what['extension'], ['mp4', 'flv', 'mkv','ogv','gifv', 'gif', 'webm'])) {
      return $what['extension'];
    }
  }
